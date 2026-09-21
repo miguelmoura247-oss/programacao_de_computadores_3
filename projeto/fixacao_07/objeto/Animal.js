af1c2538567abd2cbc701a40333a9e36fad74a29
miguelmoura247-oss <miguelmoura247@gmail.com>
2026-09-21 11:19:52 -0300

    projeto

--- projeto/fixacao_07/objeto/Animal.js
+++ projeto/fixacao_07/objeto/Animal.js
@@ -1,1 +1,18 @@
 
+class Animal{
+
+    #nome;
+    #especie;
+    #cliente;
+    #prontuario;
+    #veteriarios;
+    
+    constructor(nome,especie,cliente,prontuario,veterianarios){
+        this.#nome;
+        this.#especie;
+        this.#cliente;
+        this.#prontuario;
+        this.#veterianarios;
+    }
+}
+
--- projeto/fixacao_07/objeto/Cliente.js
+++ projeto/fixacao_07/objeto/Cliente.js
@@ -1,1 +1,36 @@
 
+class Clientes{
+   #nome;
+   #telefone;
+   #animais;
+   
+    constructor (nome,telefone,animais){
+        this.#nome;
+        this.#telefone;
+        this.#animais = [];
+    }
+
+    getNome(){
+        return this.#nome;
+    }
+    getTelefone(){
+        return this.#telefone;
+    }
+      
+
+    addAnimal(animais){
+
+        if(!(animais instanceof Animal)){
+            console.log("erro!");
+            return;
+        }
+
+        this.animais.push(objetoAnimal);
+
+        
+        objetoAnimal.dono = this; 
+    }
+    }
+}
+
+    
